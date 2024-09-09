8131d96df0974a10c2378b280bc9e245578b7c6f
Astro <joaoeusoueu13225@gmail.com>
2024-09-09 18:15:45 -0300

    caneta

---
+++ ESTUDOS/Aula7.py
@@ -0,0 +1,10 @@
+'''palavra = "radar"
+
+print('palindronmo' if palavra == palavra[::-1] else 'não é palindronomo')'''
+
+LISTA = [1, 2, 3, 4, 5, 6, 7]
+
+print(LISTA[0])
+print(LISTA[0:6])
+print(LISTA[4:])
+print(LISTA[:4])
